a754658d7e292ddc9e1bafca5bd428bbe709e89e
k0 <k0src@null.net>
2025-01-29 11:55:51 -0600

    polygon area

--- dlabs/polygon_area.cpp
+++ dlabs/polygon_area.cpp
@@ -1,7 +1,34 @@
-#include <iostream>
+#include <cstdio>
+#include <cmath>
 
 int main()
 {
+    long n_vertices;
+    scanf("%ld", &n_vertices);
 
-	return 0;
+    long* x_vertices = new long[n_vertices];
+    long* y_vertices = new long[n_vertices];
+
+    for (long i = 0; i < n_vertices; i++) {
+        scanf("%ld", &x_vertices[i]);
+        scanf("%ld", &y_vertices[i]);
+    }
+
+    long area = 0.0;
+
+    long j = n_vertices - 1;
+
+    for (long i = 0; i < n_vertices; i++) {
+        area += (x_vertices[j] + x_vertices[i]) * (y_vertices[j] - y_vertices[i]);
+        j = i;
+    }
+
+    area = std::abs(area);
+
+    printf("%ld", area);
+
+    delete[] x_vertices;
+    delete[] y_vertices;
+
+    return 0;
 }
