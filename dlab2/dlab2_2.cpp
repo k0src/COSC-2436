3a9cc1244eb2bbea8f644b236147e773d6dffb60
k0 <k0src@proton.me>
2025-03-06 22:12:26 -0600

    dlab

--- dlab2/dlab2_2.cpp
+++ dlab2/dlab2_2.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 #include <cmath>
 
+const size_t mod = 1000000007;
+
 size_t power(size_t a, size_t b) {
     if (b == 0) {
         return 1;
     }
 
     if (b % 2 == 0) {
-        return (power(a, b / 2) * power(a, b / 2)); 
+        return (power(a, b / 2) * power(a, b / 2) % mod); 
     } else {
-        return (a * power(a, b - 1));
+        return (a * power(a, b - 1) % mod);
     }
 }
 
 int main()
 {
     size_t n;
-    size_t mod = 1000000007;
     
     std::cin >> n;
 
@@ -24,7 +25,7 @@ int main()
 
     for (size_t i = 0; i < n; i++) {
         std::cin >> a >> b;
-        std::cout << power(a, b) % mod << std::endl;
+        std::cout << power(a, b) << std::endl;
     }
 
     return 0;
