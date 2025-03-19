75915fec37907ac750cc6247fd6bdf41dacb4366
k0 <k0src@null.net>
2025-03-19 13:52:51 -0500

    sorts

---
+++ Test 2/Algorithms/BucketSort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+void bucketSort(std::vector<int>& arr) {
+    int n = arr.size();
+
+    int min = *std::min_element(arr.begin(), arr.end());
+    int max = *std::max_element(arr.begin(), arr.end());
+    int range = max - min + 1;
+
+    std::vector<std::vector<int>> buckets(n);
+
+    for (int i = 0; i < n; i++) {
+        int index = ((arr[i] - min) / (double) range) * n;
+        buckets[index].push_back(arr[i]);
+    }
+
+    for (auto& bucket : buckets) {
+        std::sort(bucket.begin(), bucket.end());
+    }
+
+    int index = 0;
+    for (const auto& bucket : buckets) {
+        for (int num : bucket) {
+            arr[index++] = num;
+        }
+    }
+}
+
+int main()
+{
+    std::vector<int> arr = {38, 10, 15, 29, 19, 40, 12, 20, 30, 21};
+    bucketSort(arr);
+
+    for (int i : arr) {
+        std::cout << i << " ";
+    }
+
+    return 0;
+}
---
+++ Test 2/Algorithms/CountingSort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void countingSort(std::vector<int>& arr) {
+    int n = arr.size();
+    if (n == 0) return;
+
+    std::vector<int> res(n);
+    int max = *std::max_element(arr.begin(), arr.end());
+    std::vector<int> count_array(max + 1, 0);
+
+    for (int i = 0; i < n; i++) {
+        count_array[arr[i]]++;
+    }
+
+    for (int i = 1; i < count_array.size(); i++) {
+        count_array[i] = count_array[i - 1] + count_array[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        res[count_array[arr[i]] - 1] = arr[i];
+        count_array[arr[i]]--;
+    }
+
+    arr = res;
+}
+
+int main()
+{
+    std::vector<int> arr = {2, 5, 3, 0, 2, 3, 0, 3};
+    countingSort(arr);
+    for (int i : arr) {
+        std::cout << i << " ";
+    }
+    return 0;
+}
